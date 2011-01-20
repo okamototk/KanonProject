250829b7949784ef73c9da118b2c93dcd2c310b4
Takashi Okamoto <toraneko at gmail.com>
2011-01-20 22:16:16 +0900

    improve pom and fixed view.xml reading.

--- core/src/main/java/com/projity/graphic/configuration/BarFormat.java
+++ core/src/main/java/com/projity/graphic/configuration/BarFormat.java
@@ -1,51 +1,51 @@
 /*
-The contents of this file are subject to the Common Public Attribution License 
-Version 1.0 (the "License"); you may not use this file except in compliance with 
-the License. You may obtain a copy of the License at 
-http://www.projity.com/license . The License is based on the Mozilla Public 
-License Version 1.1 but Sections 14 and 15 have been added to cover use of 
-software over a computer network and provide for limited attribution for the 
-Original Developer. In addition, Exhibit A has been modified to be consistent 
+The contents of this file are subject to the Common Public Attribution License
+Version 1.0 (the "License"); you may not use this file except in compliance with
+the License. You may obtain a copy of the License at
+http://www.projity.com/license . The License is based on the Mozilla Public
+License Version 1.1 but Sections 14 and 15 have been added to cover use of
+software over a computer network and provide for limited attribution for the
+Original Developer. In addition, Exhibit A has been modified to be consistent
 with Exhibit B.
 
-Software distributed under the License is distributed on an "AS IS" basis, 
-WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for the 
-specific language governing rights and limitations under the License. The 
-Original Code is OpenProj. The Original Developer is the Initial Developer and 
-is Projity, Inc. All portions of the code written by Projity are Copyright (c) 
+Software distributed under the License is distributed on an "AS IS" basis,
+WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for the
+specific language governing rights and limitations under the License. The
+Original Code is OpenProj. The Original Developer is the Initial Developer and
+is Projity, Inc. All portions of the code written by Projity are Copyright (c)
 2006, 2007. All Rights Reserved. Contributors Projity, Inc.
 
-Alternatively, the contents of this file may be used under the terms of the 
-Projity End-User License Agreeement (the Projity License), in which case the 
-provisions of the Projity License are applicable instead of those above. If you 
-wish to allow use of your version of this file only under the terms of the 
-Projity License and not to allow others to use your version of this file under 
-the CPAL, indicate your decision by deleting the provisions above and replace 
-them with the notice and other provisions required by the Projity  License. If 
-you do not delete the provisions above, a recipient may use your version of this 
+Alternatively, the contents of this file may be used under the terms of the
+Projity End-User License Agreeement (the Projity License), in which case the
+provisions of the Projity License are applicable instead of those above. If you
+wish to allow use of your version of this file only under the terms of the
+Projity License and not to allow others to use your version of this file under
+the CPAL, indicate your decision by deleting the provisions above and replace
+them with the notice and other provisions required by the Projity  License. If
+you do not delete the provisions above, a recipient may use your version of this
 file under either the CPAL or the Projity License.
 
-[NOTE: The text of this license may differ slightly from the text of the notices 
-in Exhibits A and B of the license at http://www.projity.com/license. You should 
+[NOTE: The text of this license may differ slightly from the text of the notices
+in Exhibits A and B of the license at http://www.projity.com/license. You should
 use the latest text at http://www.projity.com/license for your modifications.
 You may not remove this license text from the source files.]
 
-Attribution Information: Attribution Copyright Notice: Copyright © 2006, 2007 
-Projity, Inc. Attribution Phrase (not exceeding 10 words): Powered by OpenProj, 
-an open source solution from Projity. Attribution URL: http://www.projity.com 
-Graphic Image as provided in the Covered Code as file:  openproj_logo.png with 
+Attribution Information: Attribution Copyright Notice: Copyright ï¿½ 2006, 2007
+Projity, Inc. Attribution Phrase (not exceeding 10 words): Powered by OpenProj,
+an open source solution from Projity. Attribution URL: http://www.projity.com
+Graphic Image as provided in the Covered Code as file:  openproj_logo.png with
 alternatives listed on http://www.projity.com/logo
 
-Display of Attribution Information is required in Larger Works which are defined 
-in the CPAL as a work which combines Covered Code or portions thereof with code 
-not governed by the terms of the CPAL. However, in addition to the other notice 
-obligations, all copies of the Covered Code in Executable and Source Code form 
-distributed must, as a form of attribution of the original author, include on 
-each user interface screen the "OpenProj" logo visible to all users.  The 
-OpenProj logo should be located horizontally aligned with the menu bar and left 
-justified on the top left of the screen adjacent to the File menu.  The logo 
-must be at least 100 x 25 pixels.  When users click on the "OpenProj" logo it 
-must direct them back to http://www.projity.com.  
+Display of Attribution Information is required in Larger Works which are defined
+in the CPAL as a work which combines Covered Code or portions thereof with code
+not governed by the terms of the CPAL. However, in addition to the other notice
+obligations, all copies of the Covered Code in Executable and Source Code form
+distributed must, as a form of attribution of the original author, include on
+each user interface screen the "OpenProj" logo visible to all users.  The
+OpenProj logo should be located horizontally aligned with the menu bar and left
+justified on the top left of the screen adjacent to the File menu.  The logo
+must be at least 100 x 25 pixels.  When users click on the "OpenProj" logo it
+must direct them back to http://www.projity.com.
 */
 package com.projity.graphic.configuration;
 
@@ -68,7 +68,7 @@ public class BarFormat implements NamedItem {
 	public String getCategory() {
 		return category;
 	}
-	
+
 	/**
 	 * @return Returns the end.
 	 */
@@ -95,7 +95,7 @@ public class BarFormat implements NamedItem {
 		this.from = from;
 		fromField = Configuration.getFieldFromId(from);
 	}
-	
+
 	/**
 	 * @return Returns the to.
 	 */
@@ -106,7 +106,7 @@ public class BarFormat implements NamedItem {
 	 * @param to The to to set.
 	 */
 	public void setTo(String to) {
-		this.to = to; 
+		this.to = to;
 		toField = Configuration.getFieldFromId(to);
 	}
 	/**
@@ -118,8 +118,8 @@ public class BarFormat implements NamedItem {
 	/**
 	 * @param line The line to set.
 	 */
-	public void setRow(int line) {
-		this.row = line;
+	public void setRow(Long line) {
+		this.row = line.intValue();
 	}
 	/**
 	 * @return Returns the middle.
@@ -147,7 +147,7 @@ public class BarFormat implements NamedItem {
 		this.name = name;
 	}
 
-	
+
 	public String getId() {
 		return id;
 	}
@@ -170,24 +170,24 @@ public class BarFormat implements NamedItem {
 	}
 
 
-	
-	
-	
-	
+
+
+
+
 	public int getLayer() {
 		return layer;
 	}
-	public void setLayer(int layer) {
-		this.layer = layer;
+	public void setLayer(Long layer) {
+		this.layer = layer.intValue();
 	}
-	
+
 	public String getIntervalGenerator() {
 		return intervalGenerator;
 	}
 	public void setIntervalGenerator(String intervalGenerator) {
 		this.intervalGenerator = intervalGenerator;
 	}
-	
+
 	public ScheduleIntervalGenerator getScheduleIntervalGenerator() {
 		if (intervalGenerator!=null&&scheduleIntervalGenerator==null){
 			try {
@@ -208,7 +208,7 @@ public class BarFormat implements NamedItem {
 	public void setMain(boolean main) {
 		this.main = main;
 	}
-	
+
 	public FormFormat getForm() {
 		return form;
 	}
@@ -218,38 +218,38 @@ public class BarFormat implements NamedItem {
 /**
  * Add digester events for the bar as well as the three sections.
  * The XML root is * /bar/shape
- * 	
+ *
  */
 	public static void addDigesterEvents(Digester digester){
 		// main properties of bar
 		digester.addObjectCreate("*/bar/format", "com.projity.graphic.configuration.BarFormat");
 	    digester.addSetProperties("*/bar/format");
 		digester.addSetNext("*/bar/format", "add", "com.projity.configuration.NamedItem"); // add to dictionary
-		
+
 		// start section
 		digester.addObjectCreate("*/bar/format/start", "com.projity.graphic.configuration.TexturedShape");
 	    digester.addSetProperties("*/bar/format/start");
 	    digester.addSetNext("*/bar/format/start", "setStart", "com.projity.graphic.configuration.TexturedShape");
-	    
+
 	    //middle section
 		digester.addObjectCreate("*/bar/format/middle", "com.projity.graphic.configuration.TexturedShape");
 	    digester.addSetProperties("*/bar/format/middle");
 	    digester.addSetNext("*/bar/format/middle", "setMiddle", "com.projity.graphic.configuration.TexturedShape");
-	    
+
 	    //end section
 		digester.addObjectCreate("*/bar/format/end", "com.projity.graphic.configuration.TexturedShape");
 	    digester.addSetProperties("*/bar/format/end");
 	    digester.addSetNext("*/bar/format/end", "setEnd", "com.projity.graphic.configuration.TexturedShape");
-	    
+
 	    //end section
 		digester.addObjectCreate("*/bar/format/form", "com.projity.graphic.configuration.FormFormat");
 	    digester.addSetProperties("*/bar/format/form");
 	    digester.addSetNext("*/bar/format/form", "setForm", "com.projity.graphic.configuration.FormFormat");
-		
+
 	    FormFormat.addDigesterEvents(digester);
-	    
-	}	
-	
+
+	}
+
 	String name = null;
 	String id = null;
 	int row = 0;
@@ -273,7 +273,7 @@ public class BarFormat implements NamedItem {
 	int layer=MIN_BACKGROUND_LAYER;
 	String fieldId=null;
 	Field field=null; //for annotations
-	
+
 	/**
 	 * @return Returns the category.
 	 */
@@ -289,7 +289,7 @@ public class BarFormat implements NamedItem {
 	public Field getToField() {
 		return toField;
 	}
-	
+
 	public int getNumberOfSections() {
 		int count = 0;
 		if (start != null)
@@ -300,7 +300,7 @@ public class BarFormat implements NamedItem {
 			count++;
 		return count;
 	}
-	
+
 	public String getFieldId() {
 		return fieldId;
 	}
