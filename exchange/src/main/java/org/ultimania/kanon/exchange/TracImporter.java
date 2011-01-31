c11d64f4ca9ecd4776a94690beca924dbb171844
Takashi Okamoto <toraneko at gmail.com>
2011-01-31 09:44:01 +0900

    import non-nested tasks.

--- exchange/src/main/java/org/ultimania/kanon/exchange/TracImporter.java
+++ exchange/src/main/java/org/ultimania/kanon/exchange/TracImporter.java
@@ -28,10 +28,11 @@ import antlr.collections.List;
 public class TracImporter {
 	Log log = LogFactory.getLog(TracImporter.class);
 	private TracXmlRpcClient client;
-//	private static ResourceBundle bundle= ResourceBundle.getBundle("fieldconfig");
+	// private static ResourceBundle bundle=
+	// ResourceBundle.getBundle("fieldconfig");
 	private String dateFormat = "yyyy/MM/dd";
 	private SimpleDateFormat format = new SimpleDateFormat(dateFormat);
-	private HashMap<Integer,NormalTask> map = new HashMap<Integer,NormalTask>();
+	private HashMap<Integer, NormalTask> map = new HashMap<Integer, NormalTask>();
 	private ArrayList<Ticket> ticketList;
 	private Project project;
 	private String startField = "due_assign";
@@ -39,13 +40,13 @@ public class TracImporter {
 	private String parentField = "parents";
 	private String completeField = "complete";
 
-	public TracImporter(String tracurl, String username, String password){
-		log.debug("URL: "+tracurl);
-		log.debug("Username: "+username);
-		log.debug("Password: "+password);
-		log.debug("Start field in trac: "+startField);
-		log.debug("End field in trac: "+endField);
-		log.debug("Parent field in trac: "+parentField);
+	public TracImporter(String tracurl, String username, String password) {
+		log.debug("URL: " + tracurl);
+		log.debug("Username: " + username);
+		log.debug("Password: " + password);
+		log.debug("Start field in trac: " + startField);
+		log.debug("End field in trac: " + endField);
+		log.debug("Parent field in trac: " + parentField);
 		try {
 			client = new TracXmlRpcClient(tracurl, username, password);
 		} catch (MalformedURLException e) {
@@ -54,33 +55,34 @@ public class TracImporter {
 		}
 	}
 
-	public Project getProject(){
+	public Project getProject() {
 		return project;
 	}
 
-	public String checkConnection(){
+	public String checkConnection() {
 		return client.checkConnection();
 	}
 
-	public void importByQuery(String query) throws XmlRpcException  {
+	public void importByQuery(String query) throws XmlRpcException {
 		int[] list = client.query(query);
 		ticketList = new ArrayList<Ticket>();
-		for(int id:list){
+		for (int id : list) {
 			ticketList.add(client.get(id));
 		}
 		importTicket();
 //		importHierarchy();
 	}
 
-	private long convertDate(Ticket t, String fieldName){
+	private long convertDate(Ticket t, String fieldName) {
 		String tracField = t.$(fieldName);
-		if(tracField!=null&&tracField.trim().length()!=0){
+		if (tracField != null && tracField.trim().length() != 0) {
 			try {
 				Date date = format.parse(tracField);
 				System.out.println(date);
 				return date.getTime();
 			} catch (ParseException e) {
-				log.warn("#"+t.id+": "+fieldName+"="+tracField+" is invalid format "+dateFormat,e);
+				log.warn("#" + t.id + ": " + fieldName + "=" + tracField
+						+ " is invalid format " + dateFormat, e);
 			}
 		}
 		return 0;
@@ -97,15 +99,14 @@ public class TracImporter {
 //		project.baselineWork(0,System.currentTimeMillis());
 //		project.setTemporaryLocal(true);
 
-
-		//project.setStart(0);
 		for(Ticket t:ticketList){
+			System.out.println(t.id);
 			if(log.isInfoEnabled()){
 				log.info("#"+t.id+":"+t.$("summary")+":"+t.$(startField)+"-"+t.$(endField));
 			}
 			//NormalTask task = new NormalTask(project);
 			NormalTask task = project.newNormalTaskInstance();
-			task.setProject(project);
+			task.setOwningProject(project);
 			task.setProjectId(project.getId());
 			task.setId(t.id);
 			task.setUniqueId(t.id);
@@ -114,24 +115,35 @@ public class TracImporter {
 			task.setWbs(t.$("summary"));
 
 			long start = convertDate(t,startField);
-			if(start!=0){
-				task.setStart(start);
-			}
 			long end = convertDate(t,endField);
-			if(end!=0){
-				task.setEnd(end);
-			}
-
+//			task.setDuration(end-start);
 			String tracComplete = t.$(completeField);
+			float complete = 0;
 			if(tracComplete!=null&&tracComplete.trim().length()>0){
 				try {
-					float complete = Float.parseFloat(tracComplete);
-					task.setPercentComplete(complete/100);
+					complete = Float.parseFloat(tracComplete)/100;
+					task.setPercentComplete(complete);
 				} catch(NumberFormatException e){
+					task.setPercentComplete(0);
 					log.error("#"+t.id + ": " + completeField + "='"+tracComplete+"' is invalid.");
 				}
 
+			} else {
+				task.setPercentComplete(0);
 			}
+
+			if(start!=0){
+				task.setStart(start);
+				task.getCurrentSchedule().setStart(start);
+			}
+
+			if(end!=0){
+				task.setEnd(end);
+				task.getCurrentSchedule().setEnd((long)(start+(end-start)*complete));
+//				task.getCurrentSchedule().setEnd((long)(start+(end-start)*complete));
+				task.setActualDuration(end-start);
+			}
+
 			// TODO: 以下の設定
 			// 担当者
 			// コンポーネント
@@ -140,33 +152,38 @@ public class TracImporter {
 			project.addToDefaultOutline(null ,NodeFactory.getInstance().createNode(task));
 		}
 	}
-	private void importHierarchy(){
+
+	private void importHierarchy() {
 		ArrayList<Task> hasParent = new ArrayList<Task>();
-		for(Ticket t:ticketList){
+		for (Ticket t : ticketList) {
 			String parent = t.$(parentField);
-			if(parent!=null&&parent.trim().length()>0){
+			if (parent != null && parent.trim().length() > 0) {
 				Task task = map.get(t.id);
 				try {
 					Task parentTask = map.get(Integer.parseInt(parent));
-					if (parentTask!=null) {
-						Node taskNode = NodeFactory.getInstance().createNode(task);
-						Node parentNode = NodeFactory.getInstance().createNode(parentTask);
-						project.addToDefaultOutline(parentNode ,taskNode);
+					if (parentTask != null) {
+						Node taskNode = NodeFactory.getInstance().createNode(
+								task);
+						Node parentNode = NodeFactory.getInstance().createNode(
+								parentTask);
+						project.addToDefaultOutline(parentNode, taskNode);
 						hasParent.add(task);
 					} else {
-						log.error("#"+t.id + ": " + parentField + "='"+parent+"' is not exists");
+						log.error("#" + t.id + ": " + parentField + "='"
+								+ parent + "' is not exists");
 					}
-				} catch (NumberFormatException e){
-					log.error("#"+t.id + ": " + parentField + "' is not Number format.",e);
+				} catch (NumberFormatException e) {
+					log.error("#" + t.id + ": " + parentField
+							+ "' is not Number format.", e);
 				}
 			}
 		}
-		for(Task t:hasParent){
+		for (Task t : hasParent) {
 			map.remove(t);
 		}
-		for(int id:map.keySet()){
+		for (int id : map.keySet()) {
 			Node node = NodeFactory.getInstance().createNode(map.get(id));
-			project.addToDefaultOutline(null,node);
+			project.addToDefaultOutline(null, node);
 		}
 
 	}
